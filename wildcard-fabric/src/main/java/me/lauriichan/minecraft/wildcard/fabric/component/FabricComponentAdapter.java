f46e0801a1825a44ede7ae781525fc5ef01dcbeb
Lauriichan <laura.endress@syntaxphoenix.com>
2022-02-11 22:04:51 +0100

    Fix FabricComponentAdapter
    Implemented FabricComponentAdapter because I forgot to implement it

--- wildcard-fabric/src/main/java/me/lauriichan/minecraft/wildcard/fabric/component/FabricComponentAdapter.java
+++ wildcard-fabric/src/main/java/me/lauriichan/minecraft/wildcard/fabric/component/FabricComponentAdapter.java
@@ -3,22 +3,29 @@ package me.lauriichan.minecraft.wildcard.fabric.component;
 import me.lauriichan.minecraft.wildcard.core.message.IPlatformComponentAdapter;
 import me.lauriichan.minecraft.wildcard.core.message.PlatformComponent;
 import net.minecraft.text.MutableText;
+import net.minecraft.text.LiteralText;
 
 public final class FabricComponentAdapter implements IPlatformComponentAdapter<MutableText> {
 
     @Override
-    public MutableText asHandle(PlatformComponent var1) {
-        return null;
+    public MutableText asHandle(PlatformComponent component) {
+        return ((FabricComponent) component).getHandle();
     }
 
     @Override
-    public MutableText[] asHandle(PlatformComponent... var1) {
-        return null;
+    public MutableText[] asHandle(PlatformComponent... components) {
+        LiteralText text = new LiteralText("");
+        for (PlatformComponent component : components) {
+            text.append(asHandle(component));
+        }
+        return new MutableText[] {
+            text
+        };
     }
 
     @Override
     public PlatformComponent create() {
-        return null;
+        return new FabricComponent(this);
     }
 
 }
